69168cf095cf2465ac086283ead741b034295d49
ShimGiYoon <201910852@office.seowon.ac.kr>
2024-06-09 15:18:41 +0900

    intelliJ에서 스프링부트 프로젝트 생성 및 HelloController 생성 / TODO: 자바 rentCar 옮기기

---
+++ src/main/java/com/rentsite/rentcarweb/RentCarWebApplication.java
@@ -0,0 +1,13 @@
+package com.rentsite.rentcarweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentCarWebApplication
+{
+	public static void main(String[] args)
+    {
+		SpringApplication.run(RentCarWebApplication.class, args);
+	}
+}
---
+++ src/test/java/com/rentsite/rentcarweb/RentCarWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rentsite.rentcarweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentCarWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
